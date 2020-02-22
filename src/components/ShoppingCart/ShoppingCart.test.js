bc23b4997d3c5223c1b21eace267534324640d2f
EduRomoGO <eduromogo@outlook.es>
2020-02-22 23:53:14 +0000

    test: red

--- src/components/ShoppingCart/ShoppingCart.test.js
+++ src/components/ShoppingCart/ShoppingCart.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import ShoppingCart from './ShoppingCart.js';
+import { mockFetch } from '../../utils/mocks/mockFetch.js'
+import { products } from '../../utils/mocks/mocks.js'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<ShoppingCart />);
-  const linkElement = getByText('Shopping Cart');
-  expect(linkElement).toBeInTheDocument();
+jest.mock('mockFetch');
+
+
+describe('Shopping Cart', () => {
+  afterEach(() => {
+    jest.resetAllMocks()
+  });
+
+  test('renders learn react link', () => {
+    mockFetch.mockResolvedValueOnce(products);
+    expect(mockFetch).toHaveBeenCalledTimes(1);
+
+
+    const { getByText } = render(<ShoppingCart />);
+
+    const linkElement = getByText('Shopping Cart');
+    expect(linkElement).toBeInTheDocument();
+
+    // Check that it fetches data
+    // Mock data provider
+
+    // Check data provider is called
+    // Check that list of items is printed in the screen
+    // Check loading view is shown while loading data
+    // Check that error view is shown when error happens
+
+
+  });
+
 });
---
+++ src/utils/mocks/mocks.js
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    id: 1,
+    name: 'chair',
+    price: 20,
+    currency: 'eur',
+  },
+  {
+    id: 2,
+    name: 'book',
+    price: 2,
+    currency: 'eur',
+  },
+  {
+    id: 3,
+    name: 'phone',
+    price: 24,
+    currency: 'eur',
+  },
+];
