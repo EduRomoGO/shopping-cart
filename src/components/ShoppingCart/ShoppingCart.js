29ac0ce4e4ba8a20f8f022c9ef600d5c246ab15d
EduRomoGO <eduromogo@outlook.es>
2020-02-23 10:12:14 +0000

    test: green

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -4,11 +4,14 @@ import Products from '../Products/Products.js';
 
 const ShoppingCart = () => {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const products = await mockFetch();
 
       setProducts(products);
+      setIsLoading(false);
     }
 
     fetchData()
@@ -16,7 +19,9 @@ const ShoppingCart = () => {
 
   return <section className='c-shopping-cart'>
     <header><h1>Shopping Cart</h1></header>
-    <Products products={products} />
+    {isLoading
+      ? <div data-testid='loader'>Loading...</div>
+      : <Products products={products} />}
   </section>;
 }
 
--- src/components/ShoppingCart/ShoppingCart.test.js
+++ src/components/ShoppingCart/ShoppingCart.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, findByRole, findByTestId } from '@testing-library/react';
+import { render, findByRole, findByTestId, cleanup } from '@testing-library/react';
 import ShoppingCart from './ShoppingCart.js';
 import { mockFetch } from '../../utils/mocks/mockFetch.js'
 import { products } from '../../utils/mocks/mocks.js'
@@ -7,6 +7,7 @@ import { products } from '../../utils/mocks/mocks.js'
 jest.mock('../../utils/mocks/mockFetch.js');
 
 afterEach(() => {
+  cleanup();
   jest.resetAllMocks()
 });
 
@@ -16,27 +17,30 @@ describe('Shopping Cart', () => {
     mockFetch.mockResolvedValueOnce(products);
 
 
-    const { getByText, findAllByRole, findByRole } = render(<ShoppingCart />);
+    const { getByText, findAllByRole, findByRole, getByTestId } = render(<ShoppingCart />);
 
-    // const loadingView = await findByRole('loader');
-    // expect(loadingView).toBeInTheDocument();
+    const appTitle = getByText('Shopping Cart');
+    expect(appTitle).toBeInTheDocument();
+
+
+    // Component starts loading data
+    const loadingView = getByTestId('loader');
+    expect(loadingView).toBeInTheDocument();
 
     expect(mockFetch).toHaveBeenCalledTimes(1);
 
+    // Components shows data fetched and hides loader
     const productNodesList = await findByRole('list');
     expect(productNodesList).toBeInTheDocument();
 
     const productNodes = await findAllByRole('listitem');
     expect(productNodes.length).toEqual(products.length);
 
-
-    const linkElement = getByText('Shopping Cart');
-    expect(linkElement).toBeInTheDocument();
+    expect(loadingView).not.toBeInTheDocument();
 
 
-    // Check loading view is shown while loading data
+
     // Check that error view is shown when error happens
-
 
   });
 
